a5879dd56f5a8a6d17e94fb601fb833f0ba07d6c
Khanh Nguyen <khanh.hm.nguyen03@gmail.com>
2026-01-13 13:46:27 +0700

    quizes intferface

---
+++ frontend/src/types/quiz.ts
@@ -0,0 +1,16 @@
+export interface AnswerRequest {
+    questionId: number;
+    selectedRomaji: string
+}
+
+export interface AnswerResponse {
+    correct: boolean;
+    correctRomaji: boolean;
+    message: string
+}
+
+export interface QuizResponse {
+    questionId: number;
+    character: string;
+    options: string[]
+}
